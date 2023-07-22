4ebb78ecf292bfcc1dab4957591ecab5d4942ffb
Yuya Furusawa <fullyou0798@gmail.com>
2023-07-23 01:25:13 +0900

    mainブランチとのコンフリクト解消

---
+++ src/handlers/user_challenge.rs
@@ -0,0 +1,16 @@
+use axum::{extract::Extension, http::StatusCode, response::IntoResponse, Json};
+use std::sync::Arc;
+
+use crate::repositories::user_challenge::{CompleteChallengePayload, UserChallengeRepository};
+
+pub async fn complete_challenge<T: UserChallengeRepository>(
+    Json(payload): Json<CompleteChallengePayload>,
+    Extension(repository): Extension<Arc<T>>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let row = repository
+        .complete_challnge(payload)
+        .await
+        .or(Err(StatusCode::BAD_REQUEST))?;
+
+    Ok((StatusCode::CREATED, Json(row)))
+}
--- src/repositories.rs
+++ src/repositories.rs
@@ -1,4 +1,5 @@
 pub mod challenge;
 pub mod quest;
 pub mod user;
+pub mod user_challenge;
 pub mod user_quest;
