db34e47991342a5ff3c251cccef0d2e72d4e9d0e
Yuya Furusawa <fullyou0798@gmail.com>
2023-07-31 16:18:42 +0900

    path変数の修正

--- src/handlers/user_challenge.rs
+++ src/handlers/user_challenge.rs
@@ -9,12 +9,12 @@ use std::sync::Arc;
 use crate::repositories::user_challenge::{CompleteChallengePayload, UserChallengeRepository};
 
 pub async fn complete_challenge<T: UserChallengeRepository>(
-    Path(id): Path<String>,
+    Path(challenge_id): Path<String>,
     Json(payload): Json<CompleteChallengePayload>,
     Extension(repository): Extension<Arc<T>>,
 ) -> Result<impl IntoResponse, StatusCode> {
     repository
-        .save_challenge_complete_event(payload.user_id, id)
+        .save_challenge_complete_event(payload.user_id, challenge_id)
         .await
         .or(Err(StatusCode::BAD_REQUEST))?;
 
--- src/handlers/user_quest.rs
+++ src/handlers/user_quest.rs
@@ -9,12 +9,12 @@ use std::sync::Arc;
 use crate::repositories::user_quest::{ParticipateQuestPayload, UserQuestRepository};
 
 pub async fn participate_quest<T: UserQuestRepository>(
-    Path(id): Path<String>,
+    Path(quest_id): Path<String>,
     Json(payload): Json<ParticipateQuestPayload>,
     Extension(repository): Extension<Arc<T>>,
 ) -> Result<impl IntoResponse, StatusCode> {
     repository
-        .save_quest_participate_event(payload.user_id, id)
+        .save_quest_participate_event(payload.user_id, quest_id)
         .await
         .or(Err(StatusCode::BAD_REQUEST))?;
 
