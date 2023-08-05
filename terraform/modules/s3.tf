5c2a582c0edc696d6b96a7f28823aa3ffcd6bb96
Yuya Furusawa <fullyou0798@gmail.com>
2023-08-05 19:20:17 +0900

    s3 bucketのacl修正

--- terraform/modules/s3.tf
+++ terraform/modules/s3.tf
@@ -10,9 +10,21 @@ resource "aws_s3_bucket_ownership_controls" "images_ownership_controls" {
   }
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  depends_on = [ aws_s3_bucket_ownership_controls.images_ownership_controls ]
+resource "aws_s3_bucket_public_access_block" "images_bucket_access" {
+  bucket = aws_s3_bucket.images.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "images_bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.images_ownership_controls,
+    aws_s3_bucket_public_access_block.images_bucket_access,
+  ]
 
   bucket = aws_s3_bucket.images.id
-  acl    = "private"
+  acl    = "public-read"
 }
