97f131b1da65fdb71bd3d823fd194d272f71227c
AtsukiTak <takatomgoo@gmail.com>
2023-08-30 01:47:54 +0900

    fix workflow

--- .github/workflows/check_history.sh
+++ .github/workflows/check_history.sh
@@ -8,10 +8,10 @@ set -e
 git fetch origin $GITHUB_BASE_REF
 
 # BASE_COMMITを取得する
-MERGE_BASE_COMMIT=$(git merge-base $GITHUB_BASE_REF HEAD)
+MERGE_BASE_COMMIT=$(git merge-base origin/$GITHUB_BASE_REF HEAD)
 
 # base branchの最新コミットを取得する
-BASE_HEAD_COMMIT=$(git rev-parse $GITHUB_BASE_REF)
+BASE_HEAD_COMMIT=$(git rev-parse origin/$GITHUB_BASE_REF)
 
 # Fast Forwardの可否をチェック
 if [ "$MERGE_BASE_COMMIT" = "$BASE_HEAD_COMMIT" ]; then
