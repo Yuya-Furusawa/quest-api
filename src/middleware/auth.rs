3b5e4747ad62a081897fc63ed516f71f9ac1fed8
Yuya Furusawa <fullyou0798@gmail.com>
2023-10-13 16:46:00 +0900

    ミドルウェアのテスト追加

--- src/middleware/auth.rs
+++ src/middleware/auth.rs
@@ -23,3 +23,48 @@ pub async fn auth_middleware<B>(
     }
     Err(StatusCode::UNAUTHORIZED)
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::services::user::create_jwt;
+    use axum::{
+        http::{Request, StatusCode},
+        middleware::from_fn,
+        response::IntoResponse,
+        routing::get,
+        Router,
+    };
+    use chrono::{Duration, Utc};
+    use hyper::Body;
+    use tower::ServiceExt;
+
+    async fn handler() -> impl IntoResponse {
+        StatusCode::OK
+    }
+
+    #[tokio::test]
+    async fn test_auth_middleware_with_valid_cookie() {
+        let secret_key = "secret_key".to_string();
+        let test_user_id = "test_user".to_string();
+        let now = Utc::now();
+        let iat = now.timestamp();
+        let exp = (now + Duration::hours(8)).timestamp();
+        let valid_session_token = create_jwt(&test_user_id, iat, &exp, &secret_key);
+
+        let app = Router::new()
+            .route("/", get(handler))
+            .layer(from_fn(move |req, next| {
+                auth_middleware(secret_key.clone(), req, next)
+            }));
+
+        let req = Request::builder()
+            .header("cookie", format!("session_token={}", valid_session_token))
+            .body(Body::empty())
+            .unwrap();
+
+        let res = app.oneshot(req).await.unwrap();
+
+        assert_eq!(res.status(), StatusCode::OK)
+    }
+}
